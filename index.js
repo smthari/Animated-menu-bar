2893148629423a8ba48870d074d528c2d8cc3ae1
Sumit Harijan <smtharijan@gmail.com>
2021-11-26 18:19:00 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+$(".menu-btn").click(function () {
+    $(".menu-btn").toggleClass("active");
+    $(".overlay").toggleClass("active");
+    $(".menu-container").toggleClass("active");
+  });
+  
+  /* const cursor = curDot();
+  
+  cursor.over(".line-1", {
+    borderColor: "rgba(255,255,255,.38)",
+    broderWidth: 2
+  }); */
